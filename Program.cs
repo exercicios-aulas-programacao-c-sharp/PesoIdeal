a0a342bbe741986ca8293222fe5990c79ac6474d
Diego Neri <diego@diegoneri.com>
2020-10-23 20:15:50 -0300

    Finaliza o programa PesoIdeal

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace PesoIdeal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double altura;
+            string sexo;
+            double pesoIdeal;
+
+            Console.Write("Digite sua altura em m:\t");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Sexo [M]asculino | [F]eminino:\t");
+            sexo = Console.ReadLine().ToUpper();
+            
+            if(sexo == "M")
+            {
+                pesoIdeal = altura * 72.7 - 58.0;
+                Console.WriteLine($"Seu peso ideal é {pesoIdeal:N1}kg");
+            }
+            else if(sexo == "F")
+            {
+                pesoIdeal = altura * 62.1 - 44.7;
+                Console.WriteLine($"Seu peso ideal é {pesoIdeal:N1}kg");
+            }
+            else
+            {
+                Console.WriteLine("O sexo informado é inválido");
+            }
         }
     }
 }
